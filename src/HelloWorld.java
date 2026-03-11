28f3984c77cd996bb16703e6206d9f9a4eda3cd1
fxmuru <hm5531@srmist.edu.in>
2026-03-11 13:25:56 +0530

    UC2:Print Hello name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class HelloWorld {
+    public static void main(String[] args) {
+    String name = args [0];
+    System.out.println("Hello, " + name + "!");
     }
 }
