325eb0b366c60ec4abab0662da05949a30a5bff7
fxmuru <hm5531@srmist.edu.in>
2026-03-17 13:17:34 +0530

    UC4:Print Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = "World";   
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
-        System.out.println("Hello " + name);
     }
 }
