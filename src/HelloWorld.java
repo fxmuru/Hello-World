cb22d45f40b0544994f8f630a01cecc6875369ff
fxmuru <hm5531@srmist.edu.in>
2026-04-20 12:35:14 +0530

    UC5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } 
-        else {
+            System.out.println("Hello, World!");
+        } else {
             for (String name : args) {
-                System.out.println("Hello " + name);
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
