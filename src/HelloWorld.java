b93d1b38071d295208f1e7433f098b91631c6987
fxmuru <hm5531@srmist.edu.in>
2026-04-20 12:41:33 +0530

    UC6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,13 @@ public class HelloWorld {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            String greeting = "Hello, ";
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                greeting += name + ", ";
             }
+            greeting = greeting.substring(0, greeting.length() - 2);
+            greeting += "!";
+            System.out.println(greeting);
         }
     }
 }
